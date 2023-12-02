4d63b8bb3ff054a850a9bda54d23ad5ad8a03385
hare-care <harisbajramovic2024@u.northwestern.edu>
2023-12-02 13:03:47 -0600

    changed home path

--- misc_support/gnu_setup.sh
+++ misc_support/gnu_setup.sh
@@ -1,10 +1,11 @@
 setenv LD_LIBRARY_PATH /home/ece393/tools/lib
-mkdir ~/opt
-mkdir ~/opt/riscv
+mkdir $HOME/opt
+mkdir $HOME/opt/riscv
+cd $HOME
 git clone https://github.com/riscv-collab/riscv-gnu-toolchain.git
 #git clone git@github.com:riscv-collab/riscv-gnu-toolchain.git
-cd ~/riscv-gnu-toolchain/
-setenv PATH $PATH\:~/opt/riscv/bin
+cd $HOME/riscv-gnu-toolchain/
+setenv PATH $PATH\:$HOME/opt/riscv/bin
 ./configure --prefix=../opt/riscv --with-arch=rv32gc --with-abi=ilp32d
 make clean
 make
