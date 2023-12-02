cc337ca417b40145cc2644ace6f06305456f75e8
hare-care <harisbajramovic2024@u.northwestern.edu>
2023-12-01 23:31:22 -0600

    added functional cpu

---
+++ misc_support/gnu_setup.sh
@@ -0,0 +1,10 @@
+setenv LD_LIBRARY_PATH /home/ece393/tools/lib
+mkdir ~/opt
+mkdir ~/opt/riscv
+git clone https://github.com/riscv-collab/riscv-gnu-toolchain.git
+#git clone git@github.com:riscv-collab/riscv-gnu-toolchain.git
+cd ~/riscv-gnu-toolchain/
+setenv PATH $PATH\:~/opt/riscv/bin
+./configure --prefix=../opt/riscv --with-arch=rv32gc --with-abi=ilp32d
+make clean
+make
