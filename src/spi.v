6f9e5a5e14f91e20139146eab89d1050ae837f5e
hare-care <harisbajramovic2024@u.northwestern.edu>
2024-04-18 16:17:11 -0500

    added spi and spi tb

--- src/spi.v
+++ src/spi.v
@@ -21,20 +21,167 @@ module slave_spi #(
     parameter width = 5
 )
 (
-    input sclk, cs, mosi, rstn,
-    output miso
+    input clk, sclk, cs, mosi, rstn,
+    input mem_ack, mem_accept,
+    input [31:0] data_rd_i,
+    output reg miso, start_flag, mem_d_rd_o,
+    output reg [31:0] data_adr_o, data_wr_o,
+    output reg [3:0] data_wr_en_o
+
 );
 
-reg [39:0] data_store;
+localparam s0 = 3'b000;
+localparam s1 = 3'b001;
+localparam s2 = 3'b010;
+localparam s3 = 3'b011;
+localparam s4 = 3'b100;
+localparam s5 = 3'b101;
+localparam s6 = 3'b110;
+localparam s7 = 3'b111;
+reg [2:0] state, state_c;
+
+reg [71:0] data_store;
+reg [71:0] data_store_c;
+reg [2:0] sync;
+reg [2:0] read_sync;
+reg [31:0] data_adr, data_adr_c;
+reg start_flag_c;
+reg out_flag, out_flag_c;
+wire valid;
+
+assign valid = !sync[2] & sync[1];
 
 
-always @(posedge sclk) begin
+// if !cs then accept data, else hold
+
+always @(posedge sclk or negedge rstn) begin
     if (!rstn) begin
-        data_store <= 40'b0;
+        data_store <= 72'b0;
     end
     else begin
-        data_store <= mosi;
-    end  
+        data_store[71:0] <= data_store_c[71:0];
+    end
+end
+
+always @(posedge clk or negedge rstn) begin
+    if (!rstn) begin
+        state <= s0;
+        sync <= 3'b111;
+        start_flag <= 1'b0;
+        out_flag <= 1'b0;
+        data_adr <= 32'b0;
+    end
+    else begin
+        start_flag <= start_flag_c;
+        out_flag   <= out_flag_c;
+        state      <= state_c;
+        sync[2:1]  <= sync[1:0];
+        sync[0]    <= cs;
+        data_adr <= data_adr_c;
+
+    end
+end
+
+
+always @(*) begin
+    state_c = state;
+    miso = 1'b0;
+    start_flag_c = start_flag;
+    out_flag_c = 1'b0;
+    data_adr_o = data_adr_c;
+    data_wr_o = 32'b0;
+    mem_d_rd_o = 1'b0;
+    data_wr_en_o = 4'b0;
+    data_adr_c = data_adr;
+
+    if (!cs) begin
+        data_store_c[71:1] = data_store[70:0];
+        data_store_c[0] = mosi;
+    end else if (out_flag) begin 
+        data_store_c[71:1] = data_store[70:0];
+        data_store_c[0] = 1'b0;
+        miso = data_store[71];
+    end else begin 
+        data_store_c[71:0] = data_store[71:0];
+    end
+
+    case (state)
+        s0: begin
+           // collecting data
+           if (valid) begin
+            state_c = s1;
+           end
+        end
+        s1: begin
+            // buffer phase to figure out what to do
+            case (data_store[71:64])
+                8'b01: begin
+                    state_c = s2;
+                end
+                8'b10: begin
+                    state_c = s3;
+                end
+                8'b11: begin
+                    state_c = s5;
+                end
+                default: begin
+                    state_c = s0;
+                end
+            endcase
+        end
+        s2: begin
+            // load into memory    data then address
+            data_adr_c = data_store[63:32];
+            data_wr_o = data_store[31:0];
+            data_wr_en_o = 4'b1111;
+            miso = 1'b0;
+            if (mem_ack) begin
+                state_c = s0;
+            end else begin
+                state_c = state;
+            end
+        end
+        s3: begin
+            // read out memory - getting data out of memory
+            data_adr_c = data_store[63:32];
+            state_c = s6;
+        end
+        s6: begin
+            out_flag_c = 1'b1;
+            if (out_flag) begin
+            state_c = s7;
+            end else begin
+                state_c = s6;
+            end
+        end
+        s7: begin
+            if (mem_accept) begin
+                data_store_c[71:68] = 4'b0; 
+                data_store_c[67:64] = 4'b1111;
+                data_store_c[63:32] = data_store[63:32];
+                data_store_c[31:0] = data_rd_i;
+                state_c = s4;
+            end else begin
+                state_c = s6;
+            end
+        end
+        s4: begin
+            // read out memory - sending data via SPI
+            out_flag_c = 1'b1;
+            if (!cs) begin
+                state_c = s0;
+            end
+            state_c = s4;
+        end
+        s5: begin
+            // start the processor
+            start_flag_c = 1'b1;
+            state_c = s0;
+        end
+    endcase
+
+
+
 end
 
 endmodule
--- tb/spi_tb.sv
+++ tb/spi_tb.sv
@@ -1,14 +1,17 @@
 `timescale 1ns / 1ns
 module spi_tb();
 
+localparam string INSTR_IN  = "./tb/instructions.txt";
+localparam string MEM_IN = "./tb/tcm.bin";
+
 localparam CLOCK_PERIOD = 10;
-localparam SPI_CLOCK_PERIOD = 50;
+localparam SPI_CLOCK_PERIOD = 25;
 
 logic clock = 1'b1;
 logic sclk = 1'b0;
 logic reset = 1'b1;
 logic start = 1'b0;
-logic done;
+logic done = 1'b0;
 logic sclk_flag = 1'b0;
 
 logic        in_full;
@@ -18,10 +21,64 @@ logic        out_rd_en;
 logic        out_empty;
 logic  [7:0] out_dout;
 
+logic [71:0] data_in;
+
 logic   hold_clock    = '0;
 logic   in_write_done = '0;
 logic   out_read_done = '0;
 integer out_errors    = '0;
+
+logic cs, mosi, miso, p_start;
+
+reg [7:0] mem[255:0];//131072
+integer j;
+integer f;
+
+logic          mem_i_rd_w = 1'b0; //core
+logic          mem_i_flush_w = 1'b0; //core
+logic          mem_i_invalidate_w = 1'b0; //core
+logic [ 31:0]  mem_i_pc_w = 32'H80000000; // core
+logic [ 31:0]  mem_d_addr_w; // both
+logic [ 31:0]  mem_d_data_wr_w; // both
+logic          mem_d_rd_w; // both
+logic [  3:0]  mem_d_wr_w; // both
+logic          mem_d_cacheable_w = 1'b0; //core
+logic [ 10:0]  mem_d_req_tag_w = '0; //core
+logic          mem_d_invalidate_w = 1'b0; //core
+logic          mem_d_writeback_w = 1'b0; //core
+logic          mem_d_flush_w = 1'b0; //core
+
+// mem control from spi
+logic [31:0] data_adr_o_spi, data_wr_o_spi;
+logic mem_d_rd_o_spi;
+logic [3:0] data_wr_en_o_spi;
+
+logic spi_used = 1'b1;
+
+always_comb begin
+    if (spi_used) begin
+        mem_d_addr_w = data_adr_o_spi;
+        mem_d_data_wr_w = data_wr_o_spi;
+        mem_d_rd_w = mem_d_rd_o_spi;
+        mem_d_wr_w = data_wr_en_o_spi;
+    end else begin
+        mem_d_addr_w = 32'H10;
+        mem_d_data_wr_w = '0;
+        mem_d_rd_w = '0;
+        mem_d_wr_w = '0;
+    end
+
+end
+
+logic          mem_i_accept_w;
+logic          mem_i_valid_w;
+logic          mem_i_error_w;
+logic [ 63:0]  mem_i_inst_w;
+logic [ 31:0]  mem_d_data_rd_w;
+logic          mem_d_accept_w;
+logic          mem_d_ack_w;
+logic          mem_d_error_w;
+logic [ 10:0]  mem_d_resp_tag_w;
 
 always begin 
     clock <= 1'b1;
@@ -37,30 +94,62 @@ always begin
         #(SPI_CLOCK_PERIOD/2);
         sclk <= 1'b0;
         #(SPI_CLOCK_PERIOD/2);
-    end
+    end 
 end
 
-logic cs, mosi, miso;
+// assign cs = ~sclk_flag;
+
 
 slave_spi DUT(
     .sclk(sclk),
+    .clk(clock),
     .cs(cs),
     .mosi(mosi),
     .rstn(reset),
-    .miso(miso)
+    .miso(miso),
+    .start_flag(p_start),
+    .data_rd_i(mem_d_data_rd_w),
+    .mem_d_rd_o(mem_d_rd_o_spi),
+    .data_adr_o(data_adr_o_spi),
+    .data_wr_o(data_wr_o_spi),
+    .data_wr_en_o(data_wr_en_o_spi),
+    .mem_ack(mem_d_ack_w),
+    .mem_accept(mem_d_accept_w)
 );
 
+/* REMEMBER TCM IS ACTIVE HIGH RESET */
+tcm_mem
+u_mem
+(
+    // Inputs
+     .clk_i(clock)
+    ,.rst_i(~reset)
+    ,.mem_i_rd_i(mem_i_rd_w)
+    ,.mem_i_flush_i(mem_i_flush_w)
+    ,.mem_i_invalidate_i(mem_i_invalidate_w)
+    ,.mem_i_pc_i(mem_i_pc_w)
+    ,.mem_d_addr_i(mem_d_addr_w)
+    ,.mem_d_data_wr_i(mem_d_data_wr_w)
+    ,.mem_d_rd_i(mem_d_rd_w)
+    ,.mem_d_wr_i(mem_d_wr_w)
+    ,.mem_d_cacheable_i(mem_d_cacheable_w)
+    ,.mem_d_req_tag_i(mem_d_req_tag_w)
+    ,.mem_d_invalidate_i(mem_d_invalidate_w)
+    ,.mem_d_writeback_i(mem_d_writeback_w)
+    ,.mem_d_flush_i(mem_d_flush_w)
 
+    // Outputs
+    ,.mem_i_accept_o(mem_i_accept_w)
+    ,.mem_i_valid_o(mem_i_valid_w)
+    ,.mem_i_error_o(mem_i_error_w)
+    ,.mem_i_inst_o(mem_i_inst_w)
+    ,.mem_d_data_rd_o(mem_d_data_rd_w)
+    ,.mem_d_accept_o(mem_d_accept_w)
+    ,.mem_d_ack_o(mem_d_ack_w)
+    ,.mem_d_error_o(mem_d_error_w)
+    ,.mem_d_resp_tag_o(mem_d_resp_tag_w)
+);
 
-// run sclk when the flag is up, flag up when data being transferred
-// always @(*) begin
-//     if (sclk_flag) begin
-//     sclk <= 1'b1;
-//     #(SPI_CLOCK_PERIOD/2);
-//     sclk <= 1'b0;
-//     #(SPI_CLOCK_PERIOD/2);
-//     end
-// end
 
 
 // regular clock
@@ -96,12 +185,92 @@ initial begin : tb_process
     $finish;
 end
 
+logic mem_done = 1'b0;
+
+
+initial begin : mem_in_process
+    wait(start);
+    for (j=0;j<256;j=j+1) // 131072
+        mem[j] = 0;
+
+    f = $fopen(MEM_IN, "r");
+    j = $fread(mem, f);
+    for (j=0;j<256;j=j+1)  //131072
+        u_mem.write(j, mem[j]);
+        //$display("loading in: %x\n", mem[j]);
+    mem_done = 1'b1;
+    $fclose(f);
+end
+
+
+initial begin: data_in_test
+    mosi = 1'b0;
+    wait(mem_done);
+    cs = 1'b0;
+    sclk_flag = 1'b1;
+    wait(!sclk_flag);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    @(posedge clock);
+    cs = 1'b0;
+    sclk_flag = 1'b1;
+    #5500
+    done = 1'b1;
+
+end
+
+initial begin: mosi_data_in
+    int fd;
+    int i = 0;
+    int cnt = 0;
+    fd = $fopen(INSTR_IN, "r");
+    wait(sclk_flag);
+    $fscanf(fd, "%h", data_in);
+    while (!done) begin
+        @(posedge sclk);
+        mosi = data_in[i];
+        i = i+1;
+        if (i > 72) begin
+            cnt += 1;
+            if (cnt < 2) begin
+            sclk_flag = 0;
+            end else begin
+                sclk_flag = 1'b1;
+            end
+            cs = 1'b1;
+            i = 0;
+            $fscanf(fd, "%h", data_in);
+            mosi = data_in[i];
+        end
+    end
+    $fclose(fd);
+end
+
+initial begin: miso_data_out
+    logic [71:0] miso_data_out = '0;
+    wait(sclk_flag);
+    while (!done) begin 
+        @(posedge sclk);
+        miso_data_out[71:1] = miso_data_out[70:0];
+        miso_data_out[0] = miso;
+    end
+end
+
+
+
+/*
 initial begin: sclk_testing
     wait(start);
     sclk_flag = 1'b1;
-    #1000
+    #500
+    sclk_flag = 1'b0;
+    #500
     done = 1'b1;
 end
+*/
 
 
 
